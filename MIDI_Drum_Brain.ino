52778e513fde3ed546f5b19a8f7230046730a9b4
Kalo Pilato <mista.kay@gmail.com>
2015-10-09 01:42:25 +1300

    Rough edit menu added

--- MIDI_Drum_Brain.ino
+++ MIDI_Drum_Brain.ino
@@ -18,18 +18,52 @@ byte PEDAL_VELOCITY = 1;       // Defines velocity of pedal changes
 int noteON = 144;               // 10010000 in binary, note on command
 int noteOFF = 128;              // 10000000 in binary, note off command
 
+/* MIDI Keymap Arrays (Addictive Drums Mappings) */
+int kickNote[] = {36};
+String kickName[] = {"Kick"};
+byte kickSize = 1;
+
+int snareNote[] = {37, 38, 39, 40, 41, 42, 43, 44};
+String snareName[] = {"Sn Rim", "Sn Open Hit", "Sn Rim (dbl)", "Sn Open Hit (dbl)", "Sn Shallow Rim", "Sn SideStick", "Sn Shallow Hit", "Sn Rim Click"};
+byte snareSize = 8;
+
+int tomNote[] = {72, 71, 70, 69, 68, 67, 66, 65};
+String tomName[] = {"Tom 1 Rim", "Tom 1 Open", "Tom 2 Rim", "Tom 2 Open", "Tom 3 Rim", "Tom 3 Open", "Tom 4 Rim", "Tom 4 Open"};
+byte tomSize = 8;
+
+int oHatNote[] = {54, 55, 56, 57, 58};
+String oHatName[] = {"HH Open A", "HH Open B", "HH Open C", "HH Open D", "HH Open Bell"};
+byte oHatSize = 5;
+
+int cHatNote[] = {49, 50, 51, 52, 53};
+String cHatName[] = {"HH 1 Close Tip", "HH 1 Close Shaft", "HH 2 Close Tip", "HH 2 Close Shaft", "HH Close Bell"};
+byte cHatSize = 5;
+
+int cymbalNote[] = {77, 46, 79, 81, 60, 61, 62, 47};
+String cymbalName[] = {"Cymbal 1", "Cymbal 1 (dbl)", "Cymbal 2", "Cymbal 3", "Ride Tip", "Ride Bell", "Ride Shaft", "Xtra"};
+byte cymbalSize = 8;
+
+/* Initial Drum Pad to MIDI note mappings (uses array position in MIDI Keymap Arrays) */
+byte kickPos = 0;
+byte redPos = 1;
+byte bluePos = 1;
+byte greenPos = 3;
+byte yellowPos1 = 0;
+byte yellowPos2 = 0;
+byte orangePos = 0;
+
+/* Edit Menu Arrays */
+String padNames[] = {"Kick Pedal", "Red Pad", "Blue Pad", "Green Pad", "Yellow Pad1", "Yellow Pad2", "Orange Pad"};
+String* nameArray[] = {kickName, snareName, tomName, tomName, oHatName, cHatName, cymbalName};
+int* noteArray[] = {kickNote, snareNote, tomNote, tomNote, oHatNote, cHatNote, cymbalNote};
+byte noteArraySize[] = {kickSize, snareSize, tomSize, tomSize, oHatSize, cHatSize, cymbalSize};
+byte noteArrayPos[] = {kickPos, redPos, bluePos, greenPos, yellowPos1, yellowPos2, orangePos};
+
 /* MIDI Note Values */
 int PEDAL_OFF = 59;
 int PEDAL_ON = 48;
-int KICK = 36;
-int SNARE = 38;
-int TOM1 = 71;
-int TOM2 = 69;
 int HAT_OPEN = 54;
 int HAT_CLOSED = 49;
-int CRASH_1 = 77;
-int SNARE_RIM = 37;
-int SNARE_SIDESTICK = 42;
 
 /* Hat Pedal */
 int hatPedal = 4;               // Defines which pin hat pedal is connected to
@@ -79,6 +113,12 @@ void loop(){
       pedalState = !pedalState;
     }
 
+    // Check if edit button has been pressed, if so go to the Edit Menu
+    if(buttonPressed(editButton, editPos, false)){
+      editMenu();
+      displayDefault();
+    }
+
     if(analogRead(i) > 1){
 
       byte maxVal = 0;
@@ -95,12 +135,12 @@ void loop(){
 
       // Output MIDI
       int ccNote = 0;
-      if(i == 0) ccNote = KICK;
-      if(i == 1) ccNote = SNARE;
-      if(i == 2) ccNote = TOM1;
-      if(i == 3) ccNote = TOM2;
+      if(i == 0) ccNote = kickNote[noteArrayPos[0]];
+      if(i == 1) ccNote = snareNote[noteArrayPos[1]];
+      if(i == 2) ccNote = tomNote[noteArrayPos[2]];
+      if(i == 3) ccNote = tomNote[noteArrayPos[3]];
       if(i == 4) ccNote = getHat();
-      if(i == 5) ccNote = CRASH_1;
+      if(i == 5) ccNote = cymbalNote[noteArrayPos[6]];
       MIDIoutput(ccNote, velocity);
     }
   }
@@ -120,8 +160,8 @@ void MIDIoutput(int MIDInote, int MIDIvelocity){
 
 /* Check if Hat should be open or closed and return the appropriate MIDI note */
 int getHat(){
-  if(pedalState == LOW) return HAT_CLOSED;
-  else return HAT_OPEN;
+  if(pedalState == LOW) return cHatNote[noteArrayPos[5]];
+  else return oHatNote[noteArrayPos[4]];
 }
 
 /* Checks if a button has been pressed */
@@ -141,8 +181,105 @@ boolean buttonPressed(int button, byte statePos, boolean debounce){
   return pressed;
 }
 
+/* Provides editing of MIDI notes sent by each pad and display to LCD */
+void editMenu(){
+  byte padNumber = 0;
+  //lineWrite(padNames[padNumber], "");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(padNames[padNumber]);
+
+  while(!buttonPressed(editButton, editPos, true)){
+    if(buttonPressed(forwardButton, forwardPos, true)){
+      if(padNumber < PIN_COUNT - 1) padNumber++;
+      else padNumber = 0;
+      //lineWrite(padNames[padNumber], "");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(padNames[padNumber]);
+    }
+    if(buttonPressed(backButton, backPos, true)){
+      if(padNumber > 0) padNumber--;
+      else padNumber = PIN_COUNT - 1;
+      //lineWrite(padNames[padNumber], "");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(padNames[padNumber]);
+    }
+
+    if(buttonPressed(enterButton, enterPos, true)){
+      int* currentNoteArray = noteArray[padNumber];
+      String* currentNameArray = nameArray[padNumber];
+      byte notePos = noteArrayPos[padNumber];
+      int currentNote = currentNoteArray[notePos];
+      String currentNoteName = currentNameArray[notePos];
+      String editMsg = "Edit " + padNames[padNumber] + ":";
+      //lineWrite(editMsg, currentNoteName);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(editMsg);
+      lcd.setCursor(0, 1);
+      lcd.print(currentNoteName);
+      boolean edited = false;
+
+
+      while(!edited){
+        if(buttonPressed(editButton, editPos, true)) return;
+
+        if(buttonPressed(forwardButton, forwardPos, true)){
+          if(notePos < noteArraySize[padNumber] - 1) notePos++;
+          else notePos = 0;
+          currentNoteName = currentNameArray[notePos];
+          //lineWrite(editMsg, currentNoteName);
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print(editMsg);
+          lcd.setCursor(0, 1);
+          lcd.print(currentNoteName);
+        }
+        if(buttonPressed(backButton, backPos, true)){
+          if(notePos > 0) notePos--;
+          else notePos = noteArraySize[padNumber] - 1;
+          currentNoteName = currentNameArray[notePos];
+          //lineWrite(editMsg, currentNoteName);
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print(editMsg);
+          lcd.setCursor(0, 1);
+          lcd.print(currentNoteName);
+        }
+        if(buttonPressed(enterButton, enterPos, true)){
+          noteArrayPos[padNumber] = notePos;
+          //lineWrite(editMsg, "DONE");
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print(editMsg);
+          lcd.setCursor(0, 1);
+          lcd.print("change saved");
+          delay(500);
+          edited = true;
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print(padNames[padNumber]);
+        }
+      }
+    }
+  }
+}
+
+/* Display the name of the controller */
 void displayDefault(){
+  lcd.clear();
   lcd.print("MIDI Drum Brain");
   lcd.setCursor(0, 1);
   lcd.print("v1.0");
+}
+
+/* Clear the LCD then write to it */
+void lineWrite(String message1, String message2){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(message1);
+  lcd.setCursor(0, 1);
+  lcd.print(message2);
 }
